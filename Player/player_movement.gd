43684e0e24ce3e58abdfef8cd9b151a920cad988
Lieut-Lobster <josiahskorseth@gmail.com>
2024-02-09 13:23:37 -0600

    Changed - Core Logic and implemented Enemy_Rammer
    Created Enemy_Rammer (to see what it does, go to Main.tscn and run that scene.
    
    Additionally added the grid system and made enemies spawn on it at random intervals (this will be refactored over time to adjust for pattern recognition and algorithms).

--- Player/player_movement.gd
+++ Player/player_movement.gd
@@ -1,27 +1,35 @@
 extends Area2D 
 signal hit
 
-@export var speed = 300
+@export var speed = 200
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	position = Vector2(300, 900)
+	position = Vector2(352, 800)
 	#hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO
-	
+	Global.player_position(position)
+	# Note: Game development is a little weird for the Y axis, Positive Y == Down && Negative Y == Up
+	# This is for displaying the animation changes
+	if velocity.x == 0 && velocity.y == 0:
+		$AnimatedSprite2D.animation = "Standard_visuals"
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
+		$AnimatedSprite2D.animation = "Move_right"
+	elif Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
+		$AnimatedSprite2D.animation = "Move_left"
+	elif Input.is_action_pressed("move_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
+		$AnimatedSprite2D.animation = "Move_down"
+	elif Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+		$AnimatedSprite2D.animation = "Move_up" 
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -32,21 +40,10 @@ func _process(delta):
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
-	# Note: Game development is a little weird for the Y axis, Positive Y == Down && Negative Y == Up
-	if velocity.x == 0 && velocity.y == 0:
-		$AnimatedSprite2D.animation = "Standard_visuals"
-	elif velocity.x > 0:
-		$AnimatedSprite2D.animation = "Move_right"
-	elif velocity.x < 0:
-		$AnimatedSprite2D.animation = "Move_left"
-	elif velocity.y > 0:
-		$AnimatedSprite2D.animation = "Move_down"
-	elif velocity.y < 0:
-		$AnimatedSprite2D.animation = "Move_up" 
-	
+
 func _on_body_entered(body):
-	hide() # Test, will make Player disappears after being hit
-	hit.emit()
+	#hide() # Test, will make Player disappears after being hit
+	#hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred("disabled", true)
 
---
+++ global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var player_pos
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func player_position(in_player_pos):
+	player_pos = in_player_pos
